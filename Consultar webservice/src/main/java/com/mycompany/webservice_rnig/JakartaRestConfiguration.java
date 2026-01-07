07048ada4e12c51eee03545b72f75396d17a96ab
Ioshimar <72533695+ioshimar@users.noreply.github.com>
2026-01-07 09:32:15 -0600

    Delete Consultar webservice directory

--- Consultar webservice/src/main/java/com/mycompany/webservice_rnig/JakartaRestConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.webservice_rnig;
-
-// CAMBIO: Se regresa a 'javax' para ser compatible con Tomcat 9
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class JakartaRestConfiguration extends Application {
-}
